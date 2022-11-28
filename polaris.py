de9faf2bde8005e0b581814f56bfa510d9dd3795
temitope0111 <topehnics@gmail.com>
2022-11-28 17:31:39 +0100

    --

--- polaris.py
+++ polaris.py
@@ -1,19 +1,25 @@
-import time
-
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
+import time
 
-#get the driver
-servix_obj= Service("C:\Driver\edgedriver_win64\msedgedriver.exe")
-driver=webdriver.Chrome(service=servix_obj)
-#mmaximize window
+# open web browser
+
+serv_obj = Service("C:/Driver/chromedriver_win32/chromedriver.exe")
+driver = webdriver.Chrome(service=serv_obj)
 driver.maximize_window()
-#open browser
+
+# open the page url
 driver.get("https://testing-doctor.rigourplus.com/")
+# Click on sign in button
 driver.find_element(By.XPATH, '//a[contains(@href,"/sign-in")]').click()
-driver.find_element(By.NAME, "email").send_keys("topehnics+Polaris080@gmail.com")
-driver.find_element(By.NAME, "password").send_keys("@Test123")
-time.sleep(0.2)
-driver.find_element(By.CLASS_NAME, "sc-d37aa103-0").click()
 time.sleep(0.5)
+# Enter email address
+driver.find_element(By.NAME, "email").send_keys("topehnics+Polaris080@gmail.com")
+time.sleep(0.5)
+# Enter password
+driver.find_element(By.NAME, "password").send_keys("@Test123")
+time.sleep(0.5)
+# click on Login
+
+time.sleep(5.0)
